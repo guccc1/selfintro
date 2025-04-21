ff0f1c043a1227dc578cea3abe45cc3af8199c84
guccc1 <hakukikun@gmail.com>
2025-04-21 23:57:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pyxel
 
+
 class App:
     def __init__(self):
         pyxel.init(160, 120, title="Hello Pyxel")
+        pyxel.images[0].load(0, 0, "assets/pyxel_logo_38x16.png")
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -12,5 +14,7 @@ class App:
     def draw(self):
         pyxel.cls(0)
         pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
+        pyxel.blt(61, 66, 0, 0, 0, 38, 16)
+
 
 App()
